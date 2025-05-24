70c05239ef7d111f972080c90695ad351d821295
Dmitriy Kasimov <its.d,itriy.kasimov@gmail.com>
2025-05-24 12:59:39 +0300

    created auth.interceptor.ts and injected him into provideHttpClient provider; created access-guard for all routes exclude ['/login']

---
+++ src/app/auth/auth.interceptor.ts
@@ -0,0 +1,18 @@
+import {HttpInterceptorFn} from '@angular/common/http';
+import {inject} from '@angular/core';
+import {AuthService} from './auth.service';
+
+export const authTokenInterceptor: HttpInterceptorFn = (req, next) => {
+  const token = inject(AuthService).token
+
+  if(!token)return next(req)
+
+
+  const interceptedReq = req.clone({
+    setHeaders: {
+      Authorization: `Bearer ${token}`
+    }
+  })
+
+  return next(interceptedReq)
+}
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,8 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {tap} from 'rxjs';
+import {TokenResponse} from './auth.interface';
+import {CookieService} from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +11,29 @@ export class AuthService {
   http = inject(HttpClient)
   baseApiUrl = 'https://icherniakov.ru/yt-course/auth/'
 
+  cookieService = inject(CookieService);
+
+  token: string | null = null
+  refreshToken: string | null = null
+
+  get isAuth(){
+    if(!this.token){
+      this.token = this.cookieService.get('token')
+    }
+    return !!this.token
+  }
+
   login(payload: {username: string, password: string}) {
     const fd = new FormData();
     fd.append('username', payload.username);
     fd.append('password', payload.password);
 
-    return this.http.post(`${this.baseApiUrl}token`, fd)
+    return this.http.post<TokenResponse>(`${this.baseApiUrl}token`, fd).pipe(tap(val => {
+      this.token = val.access_token
+      this.refreshToken = val.refresh_token
+
+      this.cookieService.set('token', this.token)
+      this.cookieService.set('refreshToken', this.refreshToken)
+    }))
   }
 }
