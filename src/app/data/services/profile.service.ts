6d9838597d1edada95e09a5227f1356d43e6d6c8
Dmitriy K. <its.dmitriy.kasimov@gmail.com>
2025-05-29 14:16:47 +0300

    created a layout of sidebar-component

--- src/app/data/services/profile.service.ts
+++ src/app/data/services/profile.service.ts
@@ -1,6 +1,8 @@
-import {inject, Injectable} from '@angular/core';
+import {inject, Injectable, signal} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {IProfile} from '../inerfaces/profile.interface';
+import {IPagination} from '../inerfaces/pagination.service';
+import {map, tap} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +11,22 @@ export class ProfileService {
   http = inject(HttpClient)
   baseApiUrl = 'https://icherniakov.ru/yt-course/'
 
+  me =  signal<IProfile | null>(null)
+
   getTestAccounts(){
     return this.http.get<IProfile[]>(`${this.baseApiUrl}account/test_accounts`);
   }
 
   getMe(){
-    return this.http.get<IProfile>(`${this.baseApiUrl}account/me`);
+    return this.http.get<IProfile>(`${this.baseApiUrl}account/me`).pipe(
+      tap(res => this.me.set(res))
+    )
+
+  }
+
+  getSubscribersShortList(){
+    return this.http.get<IPagination<IProfile>>(`${this.baseApiUrl}account/subscribers`).pipe(
+      map(res => res.items.slice(0,3)),
+    )
   }
 }
